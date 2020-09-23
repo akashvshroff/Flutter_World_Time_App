58c0aeab6d01906b70d8bbfcac359d344acc9b08
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-23 11:40:03 +0530

    Data passing to the home page

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -19,20 +19,56 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
     WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
     WorldTime(
-        url: 'Asia/Hong_Kong', location: 'Hong Kong', flag: 'honk_kong.png'),
+        url: 'Asia/Hong_Kong', location: 'Hong Kong', flag: 'hong_kong.png'),
     WorldTime(url: 'Asia/Kolkata', location: 'Bangalore', flag: 'india.png'),
     WorldTime(url: 'America/Denver', location: 'Calgary', flag: 'canada.png'),
   ];
 
+  void updateTime(index) async {
+    //index represents index in the locations list
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    //navigate to home screen
+    Navigator.pop(context, {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    locations.sort((a, b) => a.location.compareTo(b.location));
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey[600],
+        backgroundColor: Colors.blueGrey,
         title: Text('Choose a location'),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 2.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  updateTime(index);
+                },
+                title: Text(
+                  locations[index].location,
+                  style: TextStyle(),
+                ),
+                leading: CircleAvatar(
+                  backgroundImage:
+                      AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
