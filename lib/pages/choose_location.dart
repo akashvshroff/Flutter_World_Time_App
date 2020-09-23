f426ec2bd3ccbf1c9e66fa3c1a541e97f8998d22
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-23 10:42:40 +0530

    Created worldtime objects

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +7,23 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'germany.png'),
+    WorldTime(
+        url: 'Africa/Johannesburg',
+        location: 'Johanessburg',
+        flag: 'south_africa.png'),
+    WorldTime(url: 'Europe/Paris', location: 'Paris', flag: 'france.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(
+        url: 'Asia/Hong_Kong', location: 'Hong Kong', flag: 'honk_kong.png'),
+    WorldTime(url: 'Asia/Kolkata', location: 'Bangalore', flag: 'india.png'),
+    WorldTime(url: 'America/Denver', location: 'Calgary', flag: 'canada.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
